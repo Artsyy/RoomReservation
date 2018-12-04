147ee3542ebca2e436b5445b90aec155dfd1c98f
Artsyy <jxie9@ucmerced.edu>
2018-12-04 14:46:07 -0800

    added vince's part

--- app.py
+++ app.py
@@ -4,9 +4,112 @@ import sqlite3 as sql
 app = Flask(__name__)
 app.secret_key = "db project"
 
+reserve_id = 0
+database = "classroomManager.db"
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/reserve')
+def reserve():
+   return render_template('reserve.html')
+
+@app.route('/dReserve')
+def dReserve():
+	return render_template('dReserve.html')
+
+def validate(building, room, date, t1, t2):
+   try: 
+      conn = sql.connect(database)
+      cur = conn.cursor()
+      
+      cur.execute("""SELECT COUNT(*) FROM reservation
+          WHERE building LIKE ? AND room_number = ? 
+          AND date = ? AND start_time >= ? AND end_time <= ?;""", 
+         (building, room, date, t1, t2))
+      row1 = cur.fetchone()
+
+      cur.execute("""SELECT count(*) FROM room 
+      WHERE building LIKE ? AND room_number LIKE ?;""", (building, room))
+      row2 = cur.fetchone()
+      print(row1, row2)
+      if row1[0] == 0 and row2[0] != 0:
+         return True
+      else:
+         return False
+   except Error as e:
+      print(e)
+
+def getUser(name, email):
+   try:
+      conn = sql.connect(database)
+      cur = conn.cursor()
+      cur.execute("SELECT id FROM users WHERE email LIKE ? AND name LIKE ?", (email, name))
+      row = cur.fetchone()
+      return row[0]
+   except Error as e:
+      print(e)
+
+
+@app.route('/deleteReserve', methods = ['POST', 'GET'])
+def deleteReserve():
+   print("Deleteing Reserve")
+   date = request.form['Date']
+   start = request.form['Start']
+   end = request.form['End']
+   host = getUser(request.form['Host'], request.form['Email'])
+   print(date, start, end, host)
+   try:
+      conn = sql.connect(database)
+      cur = conn.cursor()
+      cur.execute("""DELETE FROM reservation WHERE date = ? 
+         AND start_time >= ? AND end_time <= ? AND organizer_id = ?""", 
+         (date, start, end, host) )
+      msg = "Delete Success"
+      return render_template("result.html",msg = msg)
+   except Error as e:
+      print(e)
+      msg = "Deletion Failed"
+      return render_template("result.html",msg = msg)
+
+@app.route('/makeReserve', methods = ['POST', 'GET'])
+def makeReserve():
+   global reserve_id
+   building = request.form['Building']
+   room = request.form['Room']
+   title = request.form['Event']
+   date = request.form['Date']
+   start = request.form['Start']
+   end = request.form['End']
+   comment = request.form['Comment']
+   host = getUser(request.form['Host'], request.form['Email'])
+   print(reserve_id, building, room, title, date, start, end, comment, host)
+   if request.method == 'POST' and validate(building, room, date, start, end):
+      try:
+         
+         with sql.connect(database) as con:
+            cur = con.cursor()
+            
+            # Accept invitation only from qualified users && no conflict
+            cur.execute("""INSERT INTO reservation(id, building, room_number, title, date, start_time, end_time, description, organizer_id) 
+               VALUES (?,?,?,?,?,?,?,?,?)""", (reserve_id, building, room, title, date, start, end, comment, host) )
+
+            con.commit()
+            msg = "Reservation successful"
+            reserve_id += 1
+      except:
+         con.rollback()
+         msg = "Error in Reserve Operation"
+         print(reserve_id, building, room, title, date, start, end, comment, host)
+   # Message corresponding to success/error in insert operation rendered
+      finally:
+         return render_template("result.html",msg = msg)
+         con.close()
+   else:
+      msg = "Request invalid, conflict with existing reservation. Or the desired location does not exist"
+      return render_template("result.html",msg = msg)
+
 
 @app.route('/search', methods = ['POST', 'GET'])
 def search():
@@ -36,6 +139,20 @@ def search():
 			conn.rollback()
 			return ''
 
+"""shows the the total list of all the rooms if user doesn't want to search"""
+@app.route('/list')
+def list():
+   con = sql.connect("classroomManager.db")
+   con.row_factory = sql.Row
+   
+   cur = con.cursor()
+   cur.execute("select * from room")
+   
+   rows = cur.fetchall(); 
+   return render_template("list.html", rows = rows)
+
+if __name__ == '__main__':
+   app.run(debug = True)
 
 # @app.route('/enternew')
 # def newRoom():
@@ -65,18 +182,3 @@ def search():
 # 		finally:
 # 			return render_template("result.html", msg = msg)
 # 			con.close()
-
-"""shows the the total list of all the rooms if user doesn't want to search"""
-@app.route('/list')
-def list():
-   con = sql.connect("classroomManager.db")
-   con.row_factory = sql.Row
-   
-   cur = con.cursor()
-   cur.execute("select * from room")
-   
-   rows = cur.fetchall(); 
-   return render_template("list.html", rows = rows)
-
-if __name__ == '__main__':
-   app.run(debug = True)
